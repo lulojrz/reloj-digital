05124064470afd2d352725b6bcf2b300630c8ae2
lulojrz <109257773+lulojrz@users.noreply.github.com>
2024-04-02 15:51:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function obtenerTiempo() {
+    let fecha = new Date();
+    let horas = fecha.getHours();
+    let minutos = fecha.getMinutes();
+    let segundos = fecha.getSeconds();
+
+    horas = (horas < 10) ? "0" + horas : horas;
+    minutos = (minutos < 10) ? "0" + minutos : minutos;
+
+    segundos = (segundos < 10) ? "0" + segundos : segundos;
+
+
+    let tiempo = horas + ": " + minutos + " :" + segundos
+    let reloj = document.getElementById("reloj")
+    reloj.innerHTML = tiempo
+
+}
+setInterval(() => {
+    obtenerTiempo()
+
+}, 1000);
